bf622e658ddcc71760d27a845864f5c300ee2096
Jonas Höchst <git@jonashoechst.de>
2019-01-18 22:09:49 +0100

    initial commit - BatCave

--- Readme.md
+++ Readme.md
@@ -1,47 +1,56 @@
-# TTGO LoRa + SD Card
+# BatCave
 
-The TTGO LoRa boards have some issues when using together with SD cards. Both the SX1278 and SD cards use SPI as communication bus. SPI is a protocol family can work in many different configurations.
+BatCave is a sattelite (low-power sensor) of the Nature 4.0 project. It is designed to monitor the temperature inside of tree holes in low frequencies (> 1min) over a long period of time without changing the Battery. 
 
-## ESP32: Software and Hardware SPI
+## Components 
 
-The ESP32 on the TTGO LoRa board has a builtin **hardware SPI** (HSPI) and can also do **virtual / software SPI** (VSPI) through emulation. Also the VSPI busses as well as the HSPI can be routed to **any** of the pins.
+The chosen components are listed here, each with it's important features named.
 
+- TTGO LoRa 868 MHz
+ - ESP32: WiFi, DeepSleep
+ - SX1278 LoRa Modem
+- SD Card Breakout Board (pick any)
+- PCF8523 Real-Time-Clock (exact measurements without further infrastructure)
+- DS18B20 Temperature Sensors
+ - no deviations with long cables (digital protocol)
+ - OneWire protocol (simply scaleable)
+ 
 ![TTGO LoRa Pinout](TTGO-LoRa.jpg)
 
-In the default configuration both SX1278 and the SD library uses the HSPI and thereby impede each other. 
+### Connections
 
-## Using multiple busses
+```
+#define ONE_WIRE_BUS 15
 
-In order to make the SX1278 and SD card working, two SPI busses need to be used. While experimenting with the board it also became obvious, that the SX1278 is error-prone on the hardware SPI, and did **not** work, when using the SD card on a VSPI.
-
-The final solution uses the **HSPI** bus for communication with SD and a software SPI for the SX1278. It is also an option to use two seperate software SPI busses.
-
-### SD on rerouted HSPI
-
-```c
 #define SD_CS 23
 #define SD_SCK 17
 #define SD_MOSI 12
 #define SD_MISO 13
 
-SPIClass sd_spi(HSPI);
-sd_spi.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
-
-if (!SD.begin(SD_CS, sd_spi))
-    Serial.println("SD Card: mounting failed.");
-else 
-    Serial.println("SD Card: mounted.");
-``` 
-
-### RH95 on VSPI
-
-```c
-RHSoftwareSPI sx1278_spi;
-RH_RF95 rf95(LORA_CS, LORA_IRQ, sx1278_spi);
-
-sx1278_spi.setPins(LORA_MISO, LORA_MOSI, LORA_SCK);
-if (!rf95.init()) 
-    Serial.println("LoRa Radio: init failed.");
-else
-    Serial.println("LoRa Radio: init OK!");
+#define SDA 21
+#define SCL 22
 ```
+
+## Operation
+
+The normal operation consists of:
+
+- initialize all the hardware
+- list all connected temperature sensors and read their values
+- log the readings to one CSV file per sensor
+- send the readings via WiFi to the next BaumBox
+- send the readings via LoRa to a gateway (also used as status-update
+- fall into deep sleep
+
+If one step fails, this error needs to be logged, and the rest of the operation shall continue. 
+
+- If the SD is not available, the values shall still be send.
+- If the RTC is n/a, the values shall still be logged and send, since a less accurate timestamp can still be assigned at the receiver.
+
+## Implementation Details
+
+The TTGO LoRa is kind of picky when using together with an SD breakout, the actual solution is to use multiple SPI busses, the rerouted Hardware-SPI for the SD and a Software-SPI for the SX1278. Details are explained here: [http://github.com/jonashoechst/ttgo-lora-sd](http://github.com/jonashoechst/ttgo-lora-sd).
+
+## Known bugs
+
+The deep sleep function of the ESP boards is very low power, which can lead to a shutdown of powerbanks (a function useful, when a charging smartphones is filled).
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,16 @@
 #include <SD.h>
 #include <RH_RF95.h>
 #include <RHSoftwareSPI.h>
+#include <RTClib.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Deep Sleep
+#define SENSOR_INTERVAL_uS (60 * 1000000)
+
+// OneWire Bus
+#define ONE_WIRE_BUS 15
+#define TEMPERATURE_SENSORS_MAX 4
 
 // SD SPI pins can be chosen freely, but cannot overlap with other ports!
 #define SD_CS 23
@@ -12,7 +22,7 @@
 
 #define LORA_FREQ 868.0
 
-#define LOG_PATH "/lora_recv.log"
+#define LOG_PATH "/batcave.log"
 
 // The sd card can also use a virtual SPI bus
 SPIClass sd_spi(HSPI);
@@ -21,12 +31,80 @@ SPIClass sd_spi(HSPI);
 RHSoftwareSPI sx1278_spi;
 RH_RF95 rf95(LORA_CS, LORA_IRQ, sx1278_spi);
 
+RTC_PCF8523 rtc;
+
+// DS18B20 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void log(char *logstring) {
+
+    File file = SD.open(LOG_PATH, FILE_APPEND);
+    if (!file) {
+        Serial.printf("Logging error: couldn't open logfile.");
+        Serial.printf(logstring);
+    } else {
+        file.printf("%i,%s", rtc.now().unixtime(), logstring);
+    }
+
+    file.close();
+}
+
+void addReading(uint32_t time, DeviceAddress deviceAddress, float tempC) {
+    char deviceString[17];
+    snprintf(deviceString, 17, "%02X%02X%02X%02X%02X%02X%02X%02X", 
+        deviceAddress[0], deviceAddress[1], deviceAddress[2], deviceAddress[3],
+        deviceAddress[4], deviceAddress[5], deviceAddress[6], deviceAddress[7]);
+
+    Serial.printf("%i,%s,%f\n", time, deviceString, tempC);
+
+    char csv_path[27];
+    snprintf(csv_path, 27, "/temp_%s.csv", deviceString);
+    Serial.printf("CSV: writing to %s\n", csv_path);
+
+    File csv = SD.open(csv_path, FILE_APPEND);
+    if (!csv) {
+        Serial.printf("CSV: couldn't open file %s\n", csv_path);
+    } else {
+        csv.printf("%i,%f\n", time, tempC);
+    }
+    csv.close();
+
+}
+
+void printOneWireAddress(DeviceAddress deviceAddress) {
+    for (uint8_t i = 0; i < 8; i++) {
+        Serial.print(deviceAddress[i], HEX);
+    }
+}
+
 void setup() {
+    delay(500);
+
     // Builtin LED
     pinMode(LED_BUILTIN, OUTPUT);
 
     // Serial output
     Serial.begin(115200);
+
+    // SD Card
+    sd_spi.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
+
+    if (!SD.begin(SD_CS, sd_spi))
+        Serial.println("SD Card: mounting failed.");
+    else 
+        Serial.println("SD Card: mounted.");
+
+
+    // RTC
+    rtc.begin();
+    if (!rtc.initialized()) {
+        log("RTC_INIT_FAILED");
+        Serial.println("RTC: couldn't initialize");
+    } else {
+        Serial.println("RTC: initialized");
+    }
+
 
     // LoRa: Init
     pinMode(LORA_RST, OUTPUT);
@@ -37,55 +115,52 @@ void setup() {
     // the pins for the virtual SPI explicitly to the internal connection
     sx1278_spi.setPins(LORA_MISO, LORA_MOSI, LORA_SCK);
 
-    if (!rf95.init()) 
+    if (!rf95.init()) {
+        log("LORA_INIT_FAILED");
         Serial.println("LoRa Radio: init failed.");
-    else
+    } else {
         Serial.println("LoRa Radio: init OK!");
+    }
 
     // LoRa: set frequency
-    if (!rf95.setFrequency(LORA_FREQ))
+    if (!rf95.setFrequency(LORA_FREQ)){
+        log("LORA_SET_FREQ_FAILED");
         Serial.println("LoRa Radio: setFrequency failed.");
-    else
+    } else {
         Serial.printf("LoRa Radio: freqency set to %f MHz\n", LORA_FREQ);
+    }
 
-    rf95.setModemConfig(RH_RF95::Bw125Cr45Sf128);
-
+    if (!rf95.setModemConfig(RH_RF95::Bw125Cr45Sf128)){
+        log("LORA_SET_MODEMCONFIG_FAILED");
+        Serial.println("LoRa Radio: setModemConfig failed.");
+    } else {
+        Serial.printf("LoRa Radio: modem config set to Bw125Cr45Sf128\n", LORA_FREQ);
+    }
+    
     // LoRa: Set max (23 dbm) transmission power. 
     rf95.setTxPower(23, false);
 
-    // SD Card
-    sd_spi.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
 
-    if (!SD.begin(SD_CS, sd_spi))
-        Serial.println("SD Card: mounting failed.");
-    else 
-        Serial.println("SD Card: mounted.");
+    // DS18B20: Init
+    sensors.begin();
+    sensors.requestTemperatures();
+    oneWire.reset_search();
 
+    DeviceAddress deviceAddress;
+    uint32_t unixtime = rtc.now().unixtime();
+
+    for (int i = 0; i < TEMPERATURE_SENSORS_MAX; i++) {
+        if (!oneWire.search(deviceAddress)) {
+            Serial.printf("DS18B20: Read %i sensors.\n", i);
+            break;
+        }
+
+        float reading = sensors.getTempC(deviceAddress);
+        addReading(unixtime, deviceAddress, reading);
+    }
 }
 
-uint8_t lora_buf[RH_RF95_MAX_MESSAGE_LEN];
-uint8_t lora_len;
-uint8_t receive_counter = 0;
-
 void loop() {
-    // Blink LED
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-
-    lora_len = RH_RF95_MAX_MESSAGE_LEN;
-    if (rf95.recv(lora_buf, &lora_len)) {
-        receive_counter++;
-        Serial.printf("Received LoRa message #%i (%i bytes):\n%s\n", receive_counter, lora_len, lora_buf);
-
-        File test = SD.open(LOG_PATH, FILE_APPEND);
-        if (!test) {
-            Serial.println("SD Card: writing file failed.");
-        } else {
-            Serial.printf("SD Card: appending data to %s.\n", LOG_PATH);
-            test.write(lora_buf, lora_len);
-            test.printf("\n\n");
-            test.close();
-        }
-    }
-
-    delay(100);
+    esp_sleep_enable_timer_wakeup(SENSOR_INTERVAL_uS);
+    esp_deep_sleep_start();
 }
